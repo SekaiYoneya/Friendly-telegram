46367612f6264ca653cd929adcb6b8a50db39609
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-06-03 03:20:50 +0300

    Create Pic.py

---
+++ Pic.py
@@ -0,0 +1,17 @@
+#clown... again :/ by @Sekai_Yoneya
+
+from .. import loader
+from random import choice as Yoneya
+
+
+def register(cb):
+ cb(PicPhotosMod())
+
+class PicPhotosMod(loader.Module):
+ """Фотографии из @pic."""
+ strings = {'name': 'Pic'}
+
+ async def gowcmd(self, event):
+  await event.edit('<b>Бог Войны!</b>')
+  reslt=await event.client.inline_query('pic',Yoneya(['Бог Войны','Кратос']))
+  await reslt[reslt.index(Yoneya(reslt))].click(event.to_id)
