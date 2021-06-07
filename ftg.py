e3813d4a2d975ef73f4ee32062f104e3d08b2424
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-06-07 16:52:49 +0300

    Create ftg.py

---
+++ ftg.py
@@ -0,0 +1,13 @@
+echo "1. Юзер-бот @fro0zzy - запуск 💸"
+cd ~
+cd friendly-telegram
+screen -AmdS -r python -m friendly-telegram --no-web
+echo "@fro0zzy"
+echo ""
+echo "2. Юзер-бот @DeNTLy - запуск 🔥"
+cd ~
+cd bot2
+cd friendly-telegram
+screen -AmdS -r python -m friendly-telegram --no-web
+echo "@DeNTLy"
+clear
