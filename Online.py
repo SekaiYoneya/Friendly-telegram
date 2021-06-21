1f98893407f5847fa662c00a9a95d20d42b60c6d
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-06-22 01:38:32 +0300

    Update Online.py

--- Online.py
+++ Online.py
@@ -1,11 +1,11 @@
-# Sekai_Yoneya
+# @Sekai_Yoneya
 
 from .. import loader 
 from asyncio import sleep 
  
 @loader.tds 
 class EternalOnlineMod(loader.Module): 
-    """Вечный онлайн и чтение сообщений.""" 
+    """Вечный онлайн, который будет читать сообщения в чатах.""" 
     strings = {'name': 'Online'} 
  
     async def client_ready(self, client, db): 
@@ -15,16 +15,16 @@ class EternalOnlineMod(loader.Module):
         """Включить вечный онлайн""" 
         if not self.db.get("Eternal Online", "status"): 
             self.db.set("Eternal Online", "status", True) 
-            await message.edit("Вечный онлайн включен") 
+            await message.edit("<b>Вечный онлайн включен</b>") 
             while self.db.get("Eternal Online", "status"): 
                 msg = await message.client.send_message("me", "Telegram best messenger! 🤩")
                 await msg.delete()
-                await sleep(180) 
+                await sleep(1000) 
  
         else: 
             self.db.set("Eternal Online", "status", False) 
-            await message.edit("Вечный онлайн выключен")
+            await message.edit("<b>Вечный онлайн выключен</b>")
 
     async def watcher(self, message): 
-        """Вау, это watcher, я что-то смог из него сделать. Поздравьте меня)""" 
-        await message.client.send_read_acknowledge(message.chat_id, clear_mentions=True)
+        if self.db.get("Eternal Online", "status"):
+            await message.client.send_read_acknowledge(message.chat_id, clear_mentions=True)
