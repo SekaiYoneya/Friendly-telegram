be5a4595a1f8be383ccbabf3e37fa77504c41026
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-08-05 13:14:48 +0300

    Update ftg.sh

--- ftg.sh
+++ ftg.sh
@@ -1,13 +1,13 @@
 echo "1. Юзер-бот Sekai_Yoneya - запуск 💸"
 cd /home
 cd friendly-telegram
-screen -AmdS -r python -m friendly-telegram --no-web
+screen -AmdS -r python3 -m friendly-telegram --no-web
 echo "Sekai_Yoneya"
 echo ""
 echo "2. Юзер-бот Mysterio King - запуск 🔥"
 cd ~
 cd /home/twobot
 cd friendly-telegram
-screen -AmdS -r python -m friendly-telegram --no-web
+screen -AmdS -r python3 -m friendly-telegram --no-web
 echo "Mysterio King"
 
