e47fadc816c6ec333cc3711d9943d7e5e6e768fa
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-08-28 23:29:57 +0300

    Update ftg.sh

--- ftg.sh
+++ ftg.sh
@@ -1,24 +1,4 @@
-echo "1. UserBot | Sekai_Yoneya > запуск"
-cd /home/Sekai_Yoneya
-cd friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "2. UserBot | Kajitsu_Yoneya > запуск"
-cd /home/Kajitsu_Yoneya
-cd friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "3. UserBot | Takashi_Yoneya > запуск"
-cd /home/Takashi_Yoneya
-cd friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "4. UserBot | Sakura_Yoneya > запуск"
+echo "1. UserBot | ❤️ Sakura_Yoneya > запуск"
 cd /home/Sakura_Yoneya
-cd friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "5. UserBot | Ichigo_Yoneya > запуск"
-cd /home/Ichigo_Yoneya
-cd friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "6. UserBot | Ⲁⲭⲩⲉⲃⲱυύ ⲡⲉⲗьⲙⲉⲏь. > запуск"
-cd /home/Kollet_Yoneya
 cd friendly-telegram
 screen -AmdS -r python3 -m friendly-telegram --no-web
