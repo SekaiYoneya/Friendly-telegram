b917dc9fbdf54c4e0601878a94f8e3a811d24f05
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-09-19 22:43:51 +0300

    Update ftg.sh

--- ftg.sh
+++ ftg.sh
@@ -1,5 +1,7 @@
-echo "1. UserBot | Hermano_Yoneya > запускаю! Подождите, бот включится в течении минуты."
-cd /home/Hermano_Yoneya/friendly-telegram
+echo "1. UserBot | Julia_Yoneya > запуск..."
+cd /home/Julia_Yoneya/friendly-telegram
 screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "Создатель: @Sekai_Yoneya"
+echo "Подождите, бот будет запущен в течении минуты!
+echo ""
 echo "Поддержка: @sekai_pmbot"
+echo "Создатель: @Sekai_Yoneya"
