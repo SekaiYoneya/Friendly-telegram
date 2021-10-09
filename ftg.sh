95c40524a9b9c0b7d88ac43e5007c2761bcf729a
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-10-10 01:07:09 +0300

    Update and rename ftg.sh to userbot.sh

--- ftg.sh
+++
@@ -1,7 +0,0 @@
-echo "1. UserBot | Julia_Yoneya > запуск..."
-cd /home/Julia_Yoneya/friendly-telegram
-screen -AmdS -r python3 -m friendly-telegram --no-web
-echo "Подождите, бот будет запущен в течении минуты!"
-echo ""
-echo "Поддержка: @sekai_pmbot"
-echo "Создатель: @Sekai_Yoneya"
---
+++ userbot.sh
@@ -0,0 +1,4 @@
+echo "1. UserBot | Sekai_Yoneya > запуск..."
+cd /home/ubuntu/friendly-telegram/
+screen -AmdS -r python3.9 -m friendly-telegram --no-web
+echo "Ожидайте! Бот будет запущен в течении минуты!"
