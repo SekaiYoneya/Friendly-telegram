83e1301e5b76b9f27ca3d2ac4bc9889182cbf2df
SekaiYoneya <83648830+SekaiYoneya@users.noreply.github.com>
2021-10-10 21:28:21 +0300

    Update userbot.sh

--- userbot.sh
+++ userbot.sh
@@ -1,4 +1,4 @@
 echo "1. UserBot | Sekai_Yoneya > запуск..."
-cd /home/ubuntu/friendly-telegram/
+cd /home/Sekai_Yoneya/friendly-telegram/
 screen -AmdS -r python3.9 -m friendly-telegram --no-web
 echo "Ожидайте! Бот будет запущен в течении минуты!"
